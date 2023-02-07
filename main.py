96dce4b7367f0160792ae8693a31dde63d978669
muddydog25 <etran8@gmail.com>
2023-02-06 20:49:14 -0800

    port 8092

--- main.py
+++ main.py
@@ -52,4 +52,4 @@ def activate_job():
 if __name__ == "__main__":
     # change name for testing
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///volumes/sqlite.db'
-    app.run(debug=True, host="127.0.0.1", port="8091")
+    app.run(debug=True, host="127.0.0.1", port="8092")
