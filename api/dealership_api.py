d8323a9b42d5d31d4d2976290fb0ef2ed8a64b19
20730661 <111465192+hsinaDitaM@users.noreply.github.com>
2023-02-17 21:08:02 -0800

    user input dealership

--- api/dealership_api.py
+++ api/dealership_api.py
@@ -1,6 +1,8 @@
-from flask import jsonify
+from flask import jsonify, request
 from __init__ import app
 from model.dealership_db import Dealership, session
+from sqlalchemy import create_engine
+
 
 @app.route('/dealerships')
 def get_dealerships():
@@ -8,7 +10,6 @@ def get_dealerships():
     
     response = []
     for d in dealerships:
-        # removing some sqlachemy bloat or whatever this is
         try:
             del d.__dict__["_sa_instance_state"]
         except:
@@ -16,6 +17,20 @@ def get_dealerships():
         response.append(d.__dict__)
 
     return jsonify(response)
+
+
+@app.route('/dealership_submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    address = request.form['address']
+    latitude = float(request.form['latitude'])
+    longitude = float(request.form['longitude'])
+
+    conn = engine.connect()
+    ins = dealership_table.insert().values(name=name, address=address, latitude=latitude, longitude=longitude)
+    conn.execute(ins)
+
+    return 'Data inserted successfully'
 
 if __name__ == '__main__':
     app.run()
