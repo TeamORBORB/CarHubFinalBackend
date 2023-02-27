b3e8ea3c17c3d275270c70e125cfe925fa0b8ca1
muddydog25 <etran8@gmail.com>
2023-02-26 23:44:58 -0800

    final edits + adding delete method & comments

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 from flask import render_template  # import render_template from "public" flask libraries
 
 # import "packages" from "this" project
-from __init__ import app # Definitions initialization
+from __init__ import app, db   # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
 from model.cars import initCars
@@ -12,9 +12,7 @@ from model.cars import initCars
 # setup APIs
 from api.user import user_api # Blueprint import api definition
 from api.car import cars_api
-
-from __init__ import app
-# from model.dealership_db import Dealership, session
+import api.dealership_api
 
 # register URIs
 # app.register_blueprint(joke_api) # register api routes
@@ -43,22 +41,9 @@ def car():
 @app.before_first_request
 def activate_job():
     initCars()
-    initUsers()
 
 from flask import Flask, request, jsonify, render_template
 import sqlite3
-
-# @app.route('/comments')
-# def handle_comments():
-#     comments = fetch_comments()
-#     return render_template("comments.html", comments=comments)
-
-# def delete_all_comments():
-#     conn = sqlite3.connect('comments.db')
-#     c = conn.cursor()
-#     c.execute('DELETE FROM comments')
-#     conn.commit()
-#     conn.close()
 
 @app.route('/comments', methods=['GET', 'POST'])
 def handle_comments_post_get():
@@ -66,46 +51,47 @@ def handle_comments_post_get():
         comments = fetch_comments()
         return jsonify(comments)
     if request.method == 'POST':
-        username = request.form['username']
-        comment = request.form['comment']
+        data = request.json
+        username = data['username']
+        comment = data['comment']
         insert_comment(username, comment)
         return "Comment added successfully", 201
 
 
-# def init_db():
-#     conn = sqlite3.connect('comments.db')
-#     c = conn.cursor()
-#     c.execute('''
-#         CREATE TABLE IF NOT EXISTS comments (
-#             id INTEGER PRIMARY KEY AUTOINCREMENT,
-#             username TEXT NOT NULL,
-#             comment TEXT NOT NULL,
-#             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-#         )
-#     ''')
-#     conn.commit()
-#     conn.close()
+def init_db():
+    conn = sqlite3.connect('comments.db')
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS comments (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            comment TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
-# def fetch_comments():
-#     conn = sqlite3.connect('comments.db')
-#     c = conn.cursor()
-#     c.execute('SELECT * FROM comments')
-#     comments = c.fetchall()
-#     conn.close()
-#     return comments
+def fetch_comments():
+    conn = sqlite3.connect('comments.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM comments')
+    comments = c.fetchall()
+    conn.close()
+    return comments
 
-# def insert_comment(username, comment):
-#     conn = sqlite3.connect('comments.db')
-#     c = conn.cursor()
-#     c.execute("INSERT INTO comments (username, comment) VALUES (?, ?)", (username, comment))
-#     conn.commit()
-#     conn.close()
+def insert_comment(username, comment):
+    conn = sqlite3.connect('comments.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO comments (username, comment) VALUES (?, ?)", (username, comment))
+    conn.commit()
+    conn.close()
 
 
 # this runs the application on the development server
 if __name__ == "__main__":
     # change name for testing
-    # init_db()
+    init_db()
     from flask_cors import CORS
     cors = CORS(app)
     app.run(debug=True, host="127.0.0.1", port="8055")
